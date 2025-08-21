9b7e02dd903294f5eb31cb7af4e2eb38ed9cb0ce
Rishang <rishangkumar9693@gmail.com>
2025-08-21 15:16:12 +0530

    delete shift integrated

--- components/DeleteShiftDialog.tsx
+++ components/DeleteShiftDialog.tsx
@@ -10,6 +10,8 @@ import {
   AlertDialogHeader,
   AlertDialogTitle,
 } from "@/components/ui/alert-dialog";
+import { apiResponse, DelteShift } from "@/lib/serveraction";
+import { toast } from "sonner";
 
 type prop = {
   isopen: boolean;
@@ -18,10 +20,26 @@ type prop = {
   libid: string;
 };
 
-const DeleteSpaceDialog = ({ isopen, onchangeopen, shiftid, libid }: prop) => {
-  const handleDelete = async (id: string) => {
-    try {
-    } catch (error) {}
+const DeleteSpaceDialog = ({ isopen, onchangeopen, shiftid, libid }: prop) => { 
+    const [pending,setpending]=useState<boolean>(false); 
+  const handleDelete = async (id: string) => { 
+    setpending(true)
+    try {  
+        const del= await DelteShift(shiftid,libid); 
+        
+        if(del.success){
+            toast.success("shift is delted successfully",{duration:2000})
+            onchangeopen(false)
+        }else if(!del.success){ 
+            // @ts-ignore
+            toast.error(del?.error ?? "Uncatched error ")  
+        } 
+    } catch (error) {
+      toast.error("Unexpected error");
+
+    }finally{
+         setpending(false)
+    }
   };
   return (
     <AlertDialog open={isopen}>
@@ -37,12 +55,13 @@ const DeleteSpaceDialog = ({ isopen, onchangeopen, shiftid, libid }: prop) => {
           <AlertDialogFooter>
             <AlertDialogCancel
               className="text-white bg-blue-500  hover:bg-blue-600"
-              onClick={() => onchangeopen(false)}
+              onClick={() => onchangeopen(false)} 
+              disabled={pending}
             >
               Cancel
             </AlertDialogCancel>
-            <AlertDialogAction className="text-white  bg-red-500">
-              {false ? "Deleting Space" : "Delete Space"}
+            <AlertDialogAction className="text-white  bg-red-500" disabled={pending} onClick={()=>handleDelete(shiftid)}>
+              {pending ? "Deleting Space" : "Delete Space"}
             </AlertDialogAction>
           </AlertDialogFooter>
         </AlertDialogContent>
