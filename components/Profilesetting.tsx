c25a9d023a2e5cfa2bc37b45d35e686179c43186
Rishang <rishangkumar9693@gmail.com>
2025-09-19 13:41:32 +0530

    feat: enhance user profile display in Profilesetting component and update authentication configuration

--- components/Profilesetting.tsx
+++ components/Profilesetting.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react' 
 import {
   DropdownMenu,
@@ -9,39 +10,33 @@ import {
 } from "@/components/ui/dropdown-menu" 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Button } from './ui/button' 
-import { User, Settings, LogOut } from 'lucide-react'
+import {  LogOut } from 'lucide-react'
+import { useSession ,signOut} from 'next-auth/react'
 
-const Profilesetting = () => {
+const Profilesetting = () => {  
+  const {data}=useSession() 
+   
+
   return (
     <DropdownMenu>
             <DropdownMenuTrigger asChild>
               <Button variant="ghost" className="relative h-9 w-9 rounded-full">
                 <Avatar className="h-9 w-9">
-                  <AvatarImage src="/placeholder.svg?height=36&width=36" alt="User" />
-                  <AvatarFallback>JD</AvatarFallback>
+                  <AvatarImage src={data?.user.image ?? "/defualt.png"} alt={data?.user.name} />
+                  <AvatarFallback>{data?.user.name.charAt(0)}</AvatarFallback>
                 </Avatar>
               </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent className="w-56" align="end" forceMount>
               <DropdownMenuLabel className="font-normal">
                 <div className="flex flex-col space-y-1">
-                  <p className="text-sm font-medium leading-none">John Doe</p>
+                  <p className="text-sm font-medium leading-none">{data?.user.name}</p>
                   <p className="text-xs leading-none text-muted-foreground">
-                    Library Owner
+                   {data?.user.email}
                   </p>
                 </div>
               </DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>
-                <User className="mr-2 h-4 w-4" />
-                <span>Profile</span>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <Settings className="mr-2 h-4 w-4" />
-                <span>Settings</span>
-              </DropdownMenuItem>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>
+              <DropdownMenuItem onClick={()=> signOut({redirectTo:"/"})}>
                 <LogOut className="mr-2 h-4 w-4" />
                 <span>Log out</span>
               </DropdownMenuItem>
--- lib/auth.config.ts
+++ lib/auth.config.ts
@@ -5,7 +5,8 @@ export default {
     providers: [Google({
       id: "google",
       clientId: process.env.AUTH_GOOGLE_ID as string,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET as string,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET as string, 
+      allowDangerousEmailAccountLinking: true,
     })]
 
  } satisfies NextAuthConfig
